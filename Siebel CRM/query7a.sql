1aebbdc9dbbacb1ef5bbf5d987a3b9422d4d9738
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-11-02 00:08:22 +0900

    Update query7a.sql

--- Siebel CRM/query7a.sql
+++ Siebel CRM/query7a.sql
@@ -4,6 +4,7 @@
 variable vc_applet_name VARCHAR2(75) ;
 variable vc_repository_name VARCHAR2(75) ;
 
+variable vc_applet_id VARCHAR2(15) ;
 variable vc_repository_id VARCHAR2(15) ;
 
 begin
@@ -17,10 +18,14 @@ begin
   -- Repository name is unique, See index S_REPOSITORY_U1
   SELECT row_id INTO :vc_repository_id FROM S_REPOSITORY 
   WHERE name = :vc_repository_name ;
+  --
+  SELECT row_id INTO :vc_applet_id FROM S_APPLET 
+  WHERE name = :vc_applet_name ;
 end ;
 /
 show error
 
+print :vc_applet_id :vc_applet_name
 print :vc_repository_id :vc_repository_name
 
 set markup csv on delimiter ',' quote on
@@ -29,19 +34,33 @@ show user
 print :vc_applet_name
 
 SELECT a.buscomp_name
+, a.name
+FROM S_APPLET a
+WHERE a.row_id = :vc_applet_id
+/
+
+SELECT aw.type
+, aw.web_template
+, aw.expr
+, aw.comments
+FROM S_APPL_WEB_TMPL aw  -- Applet and Web Template
+WHERE aw.applet_id = :vc_applet_id
+AND aw.repository_id = :vc_repository_id
+/
+
+SELECT lc.row_id
+, lc.list_id
 , lc.available_flg
 , lc.inactive_flg
 , lc.visible
 , lc.type 
 , lc.field_name AS field_name
-FROM S_APPLET a
-, S_LIST l
+FROM S_LIST l
 , S_LIST_COLUMN lc
-WHERE a.name = :vc_applet_name
-AND l.applet_id = a.row_id
+WHERE l.applet_id = :vc_applet_id
 AND lc.list_id = l.row_id
 AND lc.repository_id = :vc_repository_id
-ORDER BY 1, 6 ASC
+ORDER BY 6 ASC
 /
 set markup csv off
 spool off
