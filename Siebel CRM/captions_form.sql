f748956cf08da42f6db6a588251ea540dab87c91
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-11-02 23:54:56 +0900

    Create captions_form.sql

---
+++ Siebel CRM/captions_form.sql
@@ -0,0 +1,103 @@
+--
+-- 31 Oct 2022 Cheeyoung O
+-- Created to List BC Fields and Table columns
+-- with Symbol Strings on a Form Applet (Siebel 8.1.1, Oracle 11.2)
+-- This SQL scripts only works for Form Applet
+-- as different SQL statements is needed for List Applet
+-- Set NLS_LANG=AMERICAN_AMERICA.AL32UTF8 before running SQL*Plus
+-- Use SQL*Plus 12.2 or above to use "set markup csv"
+--
+
+-- input(public) variables
+variable vc_str VARCHAR2(75) ;
+variable vc_applet_name VARCHAR2(75) ;
+variable vc_bc_name VARCHAR2(75) ;
+variable vc_repository_name VARCHAR2(75) ;
+
+-- private variables.
+-- Do not update these variables. Use input variables above
+variable vc_applet_id VARCHAR2(15) ;
+variable vc_bc_id VARCHAR2(15) ;
+variable vc_bc_tab_name VARCHAR2(75) ;
+variable vc_repository_id VARCHAR2(15) ;
+
+-- update input variables here
+begin
+  :vc_str := '서비스 주문/일반수리' ;  -- Any string to identify a Form applet in a csv
+  :vc_applet_name := 'Order Entry - Order Form Applet Dashboard' ;
+  :vc_bc_name := 'Order Entry - Orders' ;
+
+  :vc_repository_name := 'Siebel Repository' ;
+end ;
+/
+show error
+
+-- initialize private variables, 
+-- when an error such as ORA-1403, See ORA-6502 for the Line number
+begin
+  -- Repository name is unique, See index S_REPOSITORY_U1
+  SELECT row_id INTO :vc_repository_id FROM S_REPOSITORY 
+  WHERE name = :vc_repository_name ;
+
+  -- BC name is unique in a repository, See index S_BUSCOMP_U1
+  SELECT row_id, table_name INTO :vc_bc_id, :vc_bc_tab_name FROM S_BUSCOMP 
+  WHERE name = :vc_bc_name AND repository_id = :vc_repository_id ;
+
+  -- Applet name is unique in a repository, See index S_APPLET_U1
+  SELECT row_id INTO :vc_applet_id FROM S_APPLET 
+  WHERE name = :vc_applet_name AND repository_id = :vc_repository_id ;
+
+  -- Control name is unique in an applet, See index S_CONTROL_U1
+end ;
+/
+show error
+
+-- for debugging
+print :vc_applet_id :vc_applet_name
+print :vc_bc_id :vc_bc_name :vc_bc_tab_name
+print :vc_repository_id :vc_repository_name
+
+-- execute the query with the private variables
+set markup csv on delimiter ',' quote on
+show user
+print :vc_applet_name :vc_bc_name :vc_bc_tab_name
+
+spool cap_form_applet.csv
+SELECT NULL AS "Sf Object"
+, NULL AS "Field API"
+--ch_tab_name
+, :vc_str
+, si.string_value
+--, c.name
+--, c.type
+, c.field_name
+, c.field_type_cd
+--, c.type
+--, c.multi_line
+, nvl2(f.col_name, :vc_bc_tab_name, NULL) AS "DB Table"
+, f.col_name "Column"
+, nvl2(f.col_name, f.join_name, NULL) AS "Join Name"
+--, c.inactive_flg
+--, c.caption
+--, c.field_retrieval_cd
+--, c.caption
+--, c.caption_ref
+--, s.name
+--, c.comments
+FROM S_CONTROL c
+, S_SYM_STR_INTL si 
+, S_SYM_STR s 
+, S_FIELD f 
+WHERE c.applet_id = :vc_applet_id
+AND s.name = c.caption_ref
+AND s.repository_id = :vc_repository_id
+AND si.lang_cd = 'KOR'
+AND si.sym_str_id = s.row_id
+AND c.repository_id = :vc_repository_id
+AND c.field_type_cd = 'BC Field'
+AND c.field_name = f.name
+AND f.buscomp_id = :vc_bc_id
+ORDER BY 4
+/
+set markup csv off
+spool off
