c0005eb889bb3ea59d4c796eca906f53bbe0a345
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-11-04 00:56:41 +0900

    Update query7a.sql

--- Siebel CRM/query7a.sql
+++ Siebel CRM/query7a.sql
@@ -4,11 +4,15 @@
 variable vc_applet_name VARCHAR2(75) ;
 variable vc_repository_name VARCHAR2(75) ;
 
+-- private variables
 variable vc_applet_id VARCHAR2(15) ;
+variable vc_bc_id VARCHAR2(15) ;
+variable vc_bc_name VARCHAR2(75) ;
+variable vc_bc_tab_name VARCHAR2(75) ;
 variable vc_repository_id VARCHAR2(15) ;
 
 begin
-  :vc_applet_name := 'SIS Account List Applet' ;
+  :vc_applet_name := 'HTW Service Request RMA AR Attachment List Applet' ;
   :vc_repository_name := 'Siebel Repository' ;
 end ;
 /
@@ -18,27 +22,29 @@ begin
   -- Repository name is unique, See index S_REPOSITORY_U1
   SELECT row_id INTO :vc_repository_id FROM S_REPOSITORY 
   WHERE name = :vc_repository_name ;
-  --
-  SELECT row_id INTO :vc_applet_id FROM S_APPLET 
-  WHERE name = :vc_applet_name ;
+
+  -- Applet name is unique in a repository, See index S_APPLET_U1
+  SELECT row_id, buscomp_name INTO :vc_applet_id, :vc_bc_name FROM S_APPLET 
+  WHERE name = :vc_applet_name AND repository_id = :vc_repository_id ;
+
+  -- BC name is unique in a repository, See index S_BUSCOMP_U1
+  SELECT row_id, table_name INTO :vc_bc_id, :vc_bc_tab_name FROM S_BUSCOMP 
+  WHERE name = :vc_bc_name AND repository_id = :vc_repository_id ;
 end ;
 /
 show error
 
 print :vc_applet_id :vc_applet_name
+print :vc_bc_id :vc_bc_name :vc_bc_tab_name 
 print :vc_repository_id :vc_repository_name
 
 set markup csv on delimiter ',' quote on
 spool query7a.csv
 show user
-print :vc_applet_name
+print :vc_applet_id :vc_applet_name
+print :vc_bc_id :vc_bc_name :vc_bc_tab_name 
 
-SELECT a.buscomp_name
-, a.name
-FROM S_APPLET a
-WHERE a.row_id = :vc_applet_id
-/
-
+/*
 SELECT aw.type
 , aw.web_template
 , aw.expr
@@ -46,7 +52,7 @@ SELECT aw.type
 FROM S_APPL_WEB_TMPL aw  -- Applet and Web Template
 WHERE aw.applet_id = :vc_applet_id
 AND aw.repository_id = :vc_repository_id
-/
+*/
 
 SELECT lc.row_id
 , lc.list_id
