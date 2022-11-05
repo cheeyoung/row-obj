4297ef8b0956f3f01568a3ba95d37c778a7bea2b
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-11-05 14:29:52 +0900

    Update captions_form.sql

--- Siebel CRM/captions_form.sql
+++ Siebel CRM/captions_form.sql
@@ -12,23 +12,34 @@
 -- input(public) variables
 variable vc_str VARCHAR2(75) ;
 variable vc_applet_name VARCHAR2(75) ;
-variable vc_bc_name VARCHAR2(75) ;
+--variable vc_bc_name VARCHAR2(75) ;
 variable vc_repository_name VARCHAR2(75) ;
 
 -- private variables.
 -- Do not update these variables. Use input variables above
 variable vc_applet_id VARCHAR2(15) ;
 variable vc_bc_id VARCHAR2(15) ;
+variable vc_bc_name VARCHAR2(75) ;
 variable vc_bc_tab_name VARCHAR2(75) ;
 variable vc_repository_id VARCHAR2(15) ;
 
 -- update input variables here
 begin
-  :vc_str := '고객정보/추가정보' ;  -- Any string to identify a Form applet in a csv
-  :vc_applet_name := 'Account Profile Applet' ;
-  :vc_bc_name := 'Account' ;
+  :vc_str := 'SWAP주문/출고/invoice' ;
+  :vc_applet_name := 'STW SWAP Order Invoice Form Applet' ;
+--  :vc_bc_name := 'Order Entry - Orders' ;
+--  :vc_str := '고객정보/추가정보' ;  -- Any string to identify a Form applet in a csv
+--  :vc_applet_name := 'Account Profile Applet' ;
+--  :vc_bc_name := 'Account' ;
 
   :vc_repository_name := 'Siebel Repository' ;
+end ;
+/
+show error
+
+-- constants, the names of the Foam Applets
+begin 
+  null ;
 end ;
 /
 show error
@@ -40,13 +51,13 @@ begin
   SELECT row_id INTO :vc_repository_id FROM S_REPOSITORY 
   WHERE name = :vc_repository_name ;
 
+  -- Applet name is unique in a repository, See index S_APPLET_U1
+  SELECT row_id, buscomp_name INTO :vc_applet_id, :vc_bc_name FROM S_APPLET 
+  WHERE name = :vc_applet_name AND repository_id = :vc_repository_id ;
+
   -- BC name is unique in a repository, See index S_BUSCOMP_U1
   SELECT row_id, table_name INTO :vc_bc_id, :vc_bc_tab_name FROM S_BUSCOMP 
   WHERE name = :vc_bc_name AND repository_id = :vc_repository_id ;
-
-  -- Applet name is unique in a repository, See index S_APPLET_U1
-  SELECT row_id INTO :vc_applet_id FROM S_APPLET 
-  WHERE name = :vc_applet_name AND repository_id = :vc_repository_id ;
 
   -- Control name is unique in an applet, See index S_CONTROL_U1
 end ;
@@ -66,15 +77,16 @@ print :vc_applet_name :vc_bc_name :vc_bc_tab_name
 
 SELECT NULL AS "Salesforce Object"
 , NULL AS "Field API Name"
-, :vc_str 
+--, :vc_str 
 , si.string_value AS "Caption"
 --, c.name
 --, c.type
-, c.field_name AS "BC Field Name"
+, :vc_bc_name AS "BC Name"  -- See f.buscomp_id = :vc_bc_id
+, c.field_name AS "Field Name"
 --, c.field_type_cd
 --, c.type
 --, c.multi_line
-, nvl2(f.col_name, :vc_bc_tab_name, NULL) AS "DB Table"
+, nvl2(f.col_name, :vc_bc_tab_name, NULL) AS "BC Table"
 , f.col_name "DB Column"
 , nvl2(f.col_name, f.join_name, NULL) AS "Join name"
 --, c.inactive_flg
@@ -97,7 +109,7 @@ AND c.repository_id = :vc_repository_id
 AND c.field_type_cd = 'BC Field'
 AND c.field_name = f.name
 AND f.buscomp_id = :vc_bc_id
-ORDER BY 4
+ORDER BY 4, 5
 /
 set markup csv off
 spool off
